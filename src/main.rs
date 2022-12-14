87128e676c7d81defe7232983f2348ed1bc7837a
xRetry <marcoschwarz.at@gmail.com>
2022-12-14 14:49:05 +0100

    Finish copy of original arm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
+extern crate ev3dev_lang_rust;
+
 mod two_link_arm;
 
-fn main() {
+use ev3dev_lang_rust::Ev3Result;
+use ev3dev_lang_rust::motors::{MediumMotor, MotorPort};
+use two_link_arm::TwoLinkArm;
 
-    println!("Hello, world!");
+fn main() -> Ev3Result<()> {
+
+    let motors = [
+        MediumMotor::get(MotorPort::OutA)?,
+        MediumMotor::get(MotorPort::OutB)?
+    ];
+
+    let lengths = [14.3, 9.];
+
+    let mut arm = TwoLinkArm{
+        motors: motors,
+        lengths: lengths,
+        dist_threshold: 0.2
+    };
+
+    let path = [[1., 1.], [-1., 1.]];
+
+    arm.follow_path(path.to_vec());
+
+    Ok(())
 }
--- src/two_link_arm.rs
+++ src/two_link_arm.rs
@@ -1,39 +1,47 @@
 use std::{thread, time};
+use ev3dev_lang_rust::motors::MediumMotor;
 
-struct Motor {}
 
 pub struct TwoLinkArm {
-    motors: [Motor; 2],
-    lengths: [f64; 2],
-    dist_threshold: f64
+    pub motors: [MediumMotor; 2],
+    pub lengths: [f64; 2],
+    pub dist_threshold: f64
 }
 
 impl TwoLinkArm {
-    pub fn follow_path(&self, path: Vec<[f64; 2]>) {
+    pub fn follow_path(&mut self, path: Vec<[f64; 2]>) {
         for x_des in path {
             self.to_coordinate(x_des);  
         } 
     }
     
-    pub fn to_coordinate(&self, x_des: [f64; 2]) {
+    pub fn to_coordinate(&mut self, x_des: [f64; 2]) {
         let angles = kin_inverse(self.lengths, x_des);
         self.set_angles(angles);
         self.wait_till_target(x_des);
     }
 
-    fn set_angles(&mut self, rad: [f64; 2]) {
+    fn set_angles(&mut self, mut rad: [f64; 2]) {
         rad[1] = -rad[1];
 
         for i in 0..2 {
-            self.motors[i].track_target(rad[i].to_degrees());
+            let ticks_per_rot = self.motors[i].get_count_per_rot().unwrap();
+            let ticks = rad[i].to_degrees() / 360. * ticks_per_rot as f64;
+            self.motors[i].run_to_abs_pos(Some(ticks as i32)).unwrap();
         }
     }
 
     fn get_angles(&self) -> [f64; 2] {
-        return [
-            self.motors[0].angle().to_radians(),
-            -self.motors[1].angle().to_radians()
-        ];
+        let mut angles = [0.; 2];
+
+        for i in 0..2 {
+            let ticks_per_rot = self.motors[i].get_count_per_rot().unwrap() as f64;
+            let proportion = self.motors[i].get_position().unwrap() as f64 / ticks_per_rot;
+            angles[i] = proportion * 360.;
+        }
+
+        angles[1] = -angles[1];
+        return angles;
     }
 
     fn wait_till_target(&self, x_des: [f64; 2]) {
